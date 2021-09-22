94c5e58a9e3c1298a16fa412c4fb2e9220c26c32
Col939 <87954615+Col939@users.noreply.github.com>
2021-09-22 12:43:37 -0400

    Update code.py

--- code.py
+++ code.py
@@ -24,7 +24,14 @@ moves = ["attack"]
 classes = ["Healer", "Warlock", "Mage"]
 cDescriptions = ["You heal hp every turn", "Your damage has a chance of being a critical strike", "Your mana recharges twice as fast"]
 
+manaMoves = ["Fireball", "Lightning Strike", "Wave of Pain"]
+manaCosts = [8, 12, 16]
+manaMoveStrenth = [5, 10, 15]
+unlockedManaMoves = ["Fireball"]
+
 fullHealth = 100
+
+fullMana = 10
 
 moveNum = 1
 
@@ -48,6 +55,7 @@ def levelUp():
     global xpNeeded
     global fullHealth
     global xpGained
+    global fullMana
     
     xpNeeded *= 1.5
     xpGained = 0
@@ -56,22 +64,31 @@ def levelUp():
     
     print("\033c")
     print("Which stat would you like to increase?")
-    x = input("a to increase attack, d to increase defense, s to increase speed, h to increase health \n")
+    x = input("a to increase attack, d to increase defense, s to increase speed, h to increase health, m to increase mana \n")
     lvlUpValue = random.randint(minLvlUp, maxLvlUp)
     if x == "a":
         player.attPower += lvlUpValue
         print("Your attack was increased by " + str(lvlUpValue))
+        
     elif x == "d":
         player.dfc += lvlUpValue
         print("Your defense was increased by " + str(lvlUpValue))
+        
     elif x == "s":
         player.speed += lvlUpValue
         print("Your speed was increased by " + str(lvlUpValue))
+        
     elif x == "h":
         player.health = fullHealth
         player.health += (lvlUpValue + 5)
-        fullHealth += lvlUpValue
+        fullHealth += (lvlUpValue + 5)
         print("Your health was set back to full and increased by " + str(lvlUpValue + 5))
+        
+    elif x == "m":
+        player.mana = fullMana
+        player.mana += (lvlUpValue + 3)
+        fullMana += (lvlUpValue + 5)
+        
     time.sleep(3)
     print("\033c")
     
@@ -118,35 +135,17 @@ def battleSequence():
         if moveNum == 1:
             print(enemy.health)
 
-        move = input("What do you do? \n" + "Type a to attack, b to block, or r to run \n")
-        print("\033c")
+        move = input("What do you do? \n" + "Type a to attack, b to block, m to use a magic attack, or r to run \nMana: " + str(player.mana) + "\n")
         
+        print("\033c")
+        moveNum += 1 
         if move == "a":
             enemy.health -= player.attPower
             turn = "e"
             time.sleep(1.5)
             print("Enemy Health: " + str(enemy.health))
-        moveNum += 1   
+          
 
-        if enemy.health <= 0:
-            if roundNum == 1:
-                xpGained = 100
-            else:
-                oldXpGained = xpGained
-                xpGained += random.randint(lvlNum, lvlNum * 100)
-            print("You killed the enemy!")
-            print("\nYou gained " + str(xpGained - oldXpGained) + "xp")
-
-            if xpNeeded <= xpGained:
-                print("You leveled up!")
-                time.sleep(2)
-                levelUp()
-            else:
-                print(str(xpNeeded - xpGained) + " is needed to level up")
-                time.sleep(2)
-                print("\033c")
-            nextBattle()
-            return
         
         elif move == "b":
             c = random.randint(1, 100);
@@ -156,7 +155,23 @@ def battleSequence():
                 block = False
             turn = "e"
             
-        
+        elif move == "m":
+            print("\033c")
+            print("Type the first and last letter of the move to activate it")
+            index = 0
+            for x in unlockedManaMoves:
+                print(x + "; Cost: " + str(manaCosts[index]))
+                index += 1
+            moveUsed = input("")
+            
+            if moveUsed == "fl":
+                enemy.health -= manaMoveStrenth[0]
+                player.mana -= manaCosts[0]
+                
+            turn = "e"
+            time.sleep(1.5)
+            print("Enemy Health: " + str(enemy.health))
+            
     if turn == "e":
         x = random.choice(moves)
         if x == "attack":
@@ -171,7 +186,26 @@ def battleSequence():
             time.sleep(1.5)
             print("\033c")
      
+    
+    if enemy.health <= 0:
+        if roundNum == 1:
+            xpGained = 100
+        else:
+            oldXpGained = xpGained
+            xpGained += random.randint(lvlNum, lvlNum * 100)
+        print("You killed the enemy!")
+        print("\nYou gained " + str(xpGained - oldXpGained) + "xp")
 
+        if xpNeeded <= xpGained:
+            print("You leveled up!")
+            time.sleep(2)
+            levelUp()
+        else:
+            print(str(xpNeeded - xpGained) + " is needed to level up")
+            time.sleep(2)
+            print("\033c")
+        nextBattle()
+        return
     
     battleSequence()
 
